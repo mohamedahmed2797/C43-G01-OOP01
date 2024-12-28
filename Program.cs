af258c6adae12052c2e9eaa8baf6e122a307f68e
mohamedahmed2797 <ma236282@gmail.com>
2024-12-28 21:19:41 +0200

    ignore case sinestive updates

--- Program.cs
+++ Program.cs
@@ -20,31 +20,31 @@ namespace oop1
             #endregion
 
             #region 2-Create an Enum called "Seas on" with the four seasons (Spring, Summer, Autumn, Winter) as its members. Write a C# program that takes a season name as input from the user and displays the corresponding month range for that season. Note range for seasons ( spring march to may , summer june to august , autumn September to November , winter December to February)
-            //bool isParsed;
-            //Season season;
-            //do
-            //{
-            //    Console.Write("please enter a season ");
-            //    isParsed = Enum.TryParse(Console.ReadLine(), out season);
-            //} while (!isParsed);
-            //switch (season)
-            //{
-            //    case Season.Spring:
-            //        Console.WriteLine("Spring: March to May");
-            //        break;
-            //    case Season.Summer:
-            //        Console.WriteLine("Summer: June to August");
-            //        break;
-            //    case Season.Autumn:
-            //        Console.WriteLine("Autumn: September to November");
-            //        break;
-            //    case Season.Winter:
-            //        Console.WriteLine("Winter: December to February");
-            //        break;
-            //    default:
-            //        Console.WriteLine("Invalid season.");
-            //        break;
-            //}
+            bool isParsed;
+            Season season;
+            do
+            {
+                Console.Write("please enter a season ");
+                isParsed = Enum.TryParse(Console.ReadLine(), ignoreCase: true, out season);
+            } while (!isParsed);
+            switch (season)
+            {
+                case Season.Spring:
+                    Console.WriteLine("Spring: March to May");
+                    break;
+                case Season.Summer:
+                    Console.WriteLine("Summer: June to August");
+                    break;
+                case Season.Autumn:
+                    Console.WriteLine("Autumn: September to November");
+                    break;
+                case Season.Winter:
+                    Console.WriteLine("Winter: December to February");
+                    break;
+                default:
+                    Console.WriteLine("Invalid season.");
+                    break;
+            }
             #endregion
             #region 3-Assign the following Permissions (Read, write, Delete, Execute) in a form of Enum .Create Variable from previous Enum to Add and Remove Permission from variable, check if specific Permission existed inside variable
             //Permissions permission = new Permissions();
@@ -61,19 +61,19 @@ namespace oop1
             //    Console.WriteLine("user has delete permission");
             //}
             //else
-            //    Console.WriteLine("user hasn''t delete permission");
+            //    Console.WriteLine("user hasn't delete permission");
             #endregion
             #region 4-Create an Enum called "Colors" with the basic colors (Red, Green, Blue) as its members. Write a C# program that takes a color name as input from the user and displays a message indicating whether the input color is a primary color or not.
-            // Colors color;
-            // bool isParsed;
-            // Console.Write("please enter a color ");
-            //isParsed = Enum.TryParse( Console.ReadLine() , out color);
-            // if (isParsed)
-            // {
-            //     Console.WriteLine($"the {color} is a primary  color : ");
-            // }
-            // else
-            //     Console.WriteLine($"this color isn''t a primary  color ");
+            //Colors color;
+            //bool isParsed;
+            //Console.Write("please enter a color ");
+            //isParsed = Enum.TryParse(Console.ReadLine(), ignoreCase:true, out color);
+            //if (isParsed)
+            //{
+            //    Console.WriteLine($"the {color} is a primary  color : ");
+            //}
+            //else
+            //    Console.WriteLine($"this color isn''t a primary  color ");
             #endregion
         }
 
